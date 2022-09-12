0dd4574474a1d90093d51e127da70cea520b8ec9
gesellkammer <eduardo.moguillansky@gmail.com>
2022-09-12 10:05:07 +0200

    add pyproject.toml

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ vampyhost = Extension('vampyhost',
                       extra_compile_args=extra_compile_args)
 
 setup (name = 'vamphost',
-       version = '1.2.0',
+       version = '1.2.1',
        python_requires=">=3.8",
        url = 'https://code.soundsoftware.ac.uk/projects/vampy-host',
        description = 'Use Vamp plugins for audio feature analysis.',
