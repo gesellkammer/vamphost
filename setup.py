e4ecfa40b280120c4b29518c9b7781fbcad30201
gesellkammer <eduardo.moguillansky@gmail.com>
2024-07-15 13:14:28 +0200

    bump version; actions

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ vampyhost = Extension('vampyhost',
                       extra_compile_args=extra_compile_args)
 
 setup (name = 'vamphost',
-       version = '1.3.0',
+       version = '1.3.1',
        python_requires=">=3.9",
        url = 'https://code.soundsoftware.ac.uk/projects/vampy-host',
        description = 'Use Vamp plugins for audio feature analysis.',
